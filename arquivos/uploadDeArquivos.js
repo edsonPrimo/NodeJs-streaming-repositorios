289c4f67dafbb27c58b65176cfefb587aa592472
Edson Primo <edson.oliveira00@hotmail.com>
2021-08-05 17:35:11 -0300

    aula 3

---
+++ arquivos/uploadDeArquivos.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+
+module.exports = (caminho, nomeDoArquivo, callBackImagemCriada) => {
+
+  const tipo = path.extname(caminho)
+
+  const tiposValidos = ['jpg', 'png', 'jpeg']
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
+
+  if (!tipoEhValido) {
+    const erro = "Tipo é inválido"
+    console.log('ERRO, tipo inválido')
+    callBackImagemCriada(erro)
+  } else {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on('finish', () => callBackImagemCriada(false , novoCaminho))
+
+  }
+
+}
